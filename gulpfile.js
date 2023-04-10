1ae7b30950f78ede987dca496c47960f2613c046
Diwakar Adhikari <diwakar.adhikari@bajajfinserv.in>
2023-04-10 16:23:39 +0530

    added sample site

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-    return src('shinobi/**/*.scss')
-        .pipe(sass())
+    return src('sass/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['shinobi/**/*.scss'], buildStyles)
+    watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
