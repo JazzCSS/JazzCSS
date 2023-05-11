1943633fb3dd6bf2315614c1f277d9179784b6f1
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2023-05-11 17:50:11 +0545

    feat: added animated text and collapsable container using pure css

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,45 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const purgecss = require('gulp-purgecss')
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 function buildStyles() {
-    return src('sass/**/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(purgecss({ content: ['*.html'] }))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(cleanCSS({
-            compatibility: 'ie11'
-        }))
-        .pipe(dest('./dist/css'))
+  return src("sass/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(
+      cleanCSS({
+        compatibility: "ie11",
+      })
+    )
+    .pipe(dest("./dist/css"));
 }
 
 function watchTask() {
-    watch(['sass/**/*.scss', '*.html'], buildStyles)
+  watch(["jazzy/**/*.scss", "sass/**/*.scss", "*.html"], buildStyles);
 }
-gulp.task('build', function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(cleanCSS({
-            compatibility: 'ie11'
-        }))
-        .pipe(gulp.dest('./dist/css'));
+gulp.task("build", function () {
+  return gulp
+    .src("src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(
+      cleanCSS({
+        compatibility: "ie11",
+      })
+    )
+    .pipe(gulp.dest("./dist/css"));
 });
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
